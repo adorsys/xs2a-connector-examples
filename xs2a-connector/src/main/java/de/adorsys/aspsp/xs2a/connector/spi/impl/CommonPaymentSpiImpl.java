5fd588cb8a71d673bfc4ee0bdccd21bc38de1836
Hryhorii Hevorkian <hhe@adorsys.com.ua>
2019-08-06 14:57:11 +0000

    #965 connector part

--- xs2a-connector/src/main/java/de/adorsys/aspsp/xs2a/connector/spi/impl/CommonPaymentSpiImpl.java
+++ xs2a-connector/src/main/java/de/adorsys/aspsp/xs2a/connector/spi/impl/CommonPaymentSpiImpl.java
@@ -16,13 +16,14 @@
 
 package de.adorsys.aspsp.xs2a.connector.spi.impl;
 
+import de.adorsys.psd2.xs2a.core.error.MessageErrorCode;
+import de.adorsys.psd2.xs2a.core.error.TppMessage;
 import de.adorsys.psd2.xs2a.spi.domain.SpiAspspConsentDataProvider;
 import de.adorsys.psd2.xs2a.spi.domain.SpiContextData;
 import de.adorsys.psd2.xs2a.spi.domain.authorisation.SpiScaConfirmation;
 import de.adorsys.psd2.xs2a.spi.domain.payment.SpiPaymentInfo;
 import de.adorsys.psd2.xs2a.spi.domain.payment.response.SpiPaymentExecutionResponse;
 import de.adorsys.psd2.xs2a.spi.domain.response.SpiResponse;
-import de.adorsys.psd2.xs2a.spi.domain.response.SpiResponseStatus;
 import de.adorsys.psd2.xs2a.spi.service.CommonPaymentSpi;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
@@ -34,14 +35,14 @@ public class CommonPaymentSpiImpl implements CommonPaymentSpi {
 	public @NotNull SpiResponse<SpiPaymentExecutionResponse> executePaymentWithoutSca(@NotNull SpiContextData contextData,
 																					  @NotNull SpiPaymentInfo payment,
 																					  @NotNull SpiAspspConsentDataProvider aspspConsentDataProvider) {
-		return SpiResponse.<SpiPaymentExecutionResponse>builder().fail(SpiResponseStatus.NOT_SUPPORTED);
+		return SpiResponse.<SpiPaymentExecutionResponse>builder().error(new TppMessage(MessageErrorCode.SERVICE_NOT_SUPPORTED, "Service is not supported")).build();
 	}
 
 	@Override
 	public @NotNull SpiResponse<SpiPaymentExecutionResponse> verifyScaAuthorisationAndExecutePayment(
 			@NotNull SpiContextData contextData, @NotNull SpiScaConfirmation spiScaConfirmation,
 			@NotNull SpiPaymentInfo payment, @NotNull SpiAspspConsentDataProvider aspspConsentDataProvider) {
-		return SpiResponse.<SpiPaymentExecutionResponse>builder().fail(SpiResponseStatus.NOT_SUPPORTED);
+		return SpiResponse.<SpiPaymentExecutionResponse>builder().error(new TppMessage(MessageErrorCode.SERVICE_NOT_SUPPORTED, "Service is not supported")).build();
 	}
 
 }
