1a26bc77b09733c14c14dfea3caf9f4a2736e9b6
Serhii Shymkov <ssh@adorsys.com.ua>
2019-10-30 22:26:23 +0000

    Support integrated and pre-step oauth based on custom header

--- xs2a-connector-remote/src/test/java/de/adorsys/aspsp/xs2a/remote/connector/oauth/TokenAuthenticationFilterTest.java
+++ xs2a-connector-remote/src/test/java/de/adorsys/aspsp/xs2a/remote/connector/oauth/TokenAuthenticationFilterTest.java
@@ -1,9 +1,12 @@
 package de.adorsys.aspsp.xs2a.remote.connector.oauth;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import de.adorsys.ledgers.middleware.api.domain.um.BearerTokenTO;
-import de.adorsys.psd2.mapper.Xs2aObjectMapper;
+import de.adorsys.psd2.aspsp.profile.domain.AspspSettings;
+import de.adorsys.psd2.aspsp.profile.domain.pis.PisAspspProfileSetting;
+import de.adorsys.psd2.aspsp.profile.service.AspspProfileService;
 import de.adorsys.psd2.xs2a.core.error.MessageErrorCode;
+import de.adorsys.psd2.xs2a.core.profile.PaymentType;
+import de.adorsys.psd2.xs2a.core.profile.ScaApproach;
 import de.adorsys.psd2.xs2a.exception.MessageCategory;
 import de.adorsys.psd2.xs2a.web.error.TppErrorMessageBuilder;
 import de.adorsys.psd2.xs2a.web.filter.TppErrorMessage;
@@ -22,6 +25,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.*;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
@@ -29,8 +33,10 @@ import static org.mockito.Mockito.*;
 @RunWith(MockitoJUnitRunner.class)
 public class TokenAuthenticationFilterTest {
     private static final String OAUTH_MODE_HEADER_NAME = "X-OAUTH-PREFERRED";
+    private static final String OAUTH_MODE_INTEGRATED = "integrated";
     private static final String BEARER_TOKEN_PREFIX = "Bearer ";
     private static final String BEARER_TOKEN_VALUE = "some_token";
+    private static final String SERVLET_PATH = "/v1/accounts";
 
     @Mock
     private TppErrorMessageBuilder tppErrorMessageBuilder;
@@ -43,19 +49,29 @@ public class TokenAuthenticationFilterTest {
     private HttpServletResponse httpServletResponse;
     @Mock
     private FilterChain filterChain;
+    @Mock
+    private OauthDataHolder oauthDataHolder;
+    @Mock
+    private AspspProfileService aspspProfileService;
 
     private TokenAuthenticationFilter tokenAuthenticationFilter;
 
     @Before
     public void setUp() {
-        tokenAuthenticationFilter = new TokenAuthenticationFilter(OAUTH_MODE_HEADER_NAME, new Xs2aObjectMapper(), tppErrorMessageBuilder, tokenValidationService);
+        tokenAuthenticationFilter = new TokenAuthenticationFilter(OAUTH_MODE_HEADER_NAME, tppErrorMessageBuilder, tokenValidationService, aspspProfileService, oauthDataHolder);
     }
 
     @Test
     public void doFilterInternalTest_withValidToken() throws ServletException, IOException {
         // Given
-        when(httpServletRequest.getHeader(OAUTH_MODE_HEADER_NAME)).thenReturn("true");
-        when(httpServletRequest.getServletPath()).thenReturn("/v1/accounts");
+        when(httpServletRequest.getHeader(OAUTH_MODE_HEADER_NAME)).thenReturn(OAUTH_MODE_INTEGRATED);
+        when(httpServletRequest.getServletPath()).thenReturn(SERVLET_PATH);
+
+        when(aspspProfileService.getScaApproaches()).thenReturn(Arrays.asList(ScaApproach.REDIRECT, ScaApproach.OAUTH));
+
+        Map<PaymentType, Set<String>> supportedPaymentTypeAndProductMatrix = new HashMap<>();
+        supportedPaymentTypeAndProductMatrix.put(PaymentType.SINGLE, Collections.singleton("sepa-credit-transfers"));
+        when(aspspProfileService.getAspspSettings()).thenReturn(new AspspSettings(null, new PisAspspProfileSetting(supportedPaymentTypeAndProductMatrix, 0, 0, false, null, null), null, null));
 
         String authorisationTokenValue = BEARER_TOKEN_PREFIX + BEARER_TOKEN_VALUE;
 
@@ -79,7 +95,7 @@ public class TokenAuthenticationFilterTest {
     public void doFilter_withoutOauthHeader_shouldSkipValidation() throws ServletException, IOException {
         // Given
         when(httpServletRequest.getHeader(OAUTH_MODE_HEADER_NAME)).thenReturn(null);
-        when(httpServletRequest.getServletPath()).thenReturn("/v1/accounts");
+        when(httpServletRequest.getServletPath()).thenReturn(SERVLET_PATH);
 
         // When
         tokenAuthenticationFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
@@ -89,23 +105,41 @@ public class TokenAuthenticationFilterTest {
     }
 
     @Test
-    public void doFilter_withoutInvalidOauthHeader_shouldSkipValidation() throws ServletException, IOException {
+    public void doFilter_withInvalidOauthHeader_shouldReturnError() throws ServletException, IOException {
         // Given
         when(httpServletRequest.getHeader(OAUTH_MODE_HEADER_NAME)).thenReturn("invalid value");
-        when(httpServletRequest.getServletPath()).thenReturn("/v1/accounts");
+        when(httpServletRequest.getServletPath()).thenReturn(SERVLET_PATH);
+
+        when(aspspProfileService.getScaApproaches()).thenReturn(Arrays.asList(ScaApproach.REDIRECT, ScaApproach.OAUTH));
+
+        PrintWriter mockWriter = Mockito.mock(PrintWriter.class);
+        when(httpServletResponse.getWriter()).thenReturn(mockWriter);
+
+        TppErrorMessage tppErrorMessage = new TppErrorMessage(MessageCategory.ERROR, MessageErrorCode.FORMAT_ERROR, "some message");
+        when(tppErrorMessageBuilder.buildTppErrorMessage(MessageCategory.ERROR, MessageErrorCode.FORMAT_ERROR))
+                .thenReturn(tppErrorMessage);
 
         // When
         tokenAuthenticationFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
 
-        verify(tokenValidationService, never()).validate(anyString());
-        verify(filterChain).doFilter(httpServletRequest, httpServletResponse);
+        // Then
+        verify(httpServletResponse).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        verify(httpServletResponse).setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        verify(mockWriter).print(tppErrorMessage.toString());
+        verify(filterChain, never()).doFilter(any(), any());
     }
 
     @Test
     public void doFilterInternalTest_withNoTokenInHeader_shouldReturnError() throws ServletException, IOException {
         // Given
-        when(httpServletRequest.getHeader(OAUTH_MODE_HEADER_NAME)).thenReturn("true");
-        when(httpServletRequest.getServletPath()).thenReturn("/v1/accounts");
+        when(httpServletRequest.getHeader(OAUTH_MODE_HEADER_NAME)).thenReturn(OAUTH_MODE_INTEGRATED);
+        when(httpServletRequest.getServletPath()).thenReturn(SERVLET_PATH);
+
+        when(aspspProfileService.getScaApproaches()).thenReturn(Arrays.asList(ScaApproach.REDIRECT, ScaApproach.OAUTH));
+
+        Map<PaymentType, Set<String>> supportedPaymentTypeAndProductMatrix = new HashMap<>();
+        supportedPaymentTypeAndProductMatrix.put(PaymentType.SINGLE, Collections.singleton("sepa-credit-transfers"));
+        when(aspspProfileService.getAspspSettings()).thenReturn(new AspspSettings(null, new PisAspspProfileSetting(supportedPaymentTypeAndProductMatrix, 0, 0, false, null, null), null, null));
 
         PrintWriter mockWriter = Mockito.mock(PrintWriter.class);
         when(httpServletResponse.getWriter()).thenReturn(mockWriter);
@@ -114,25 +148,29 @@ public class TokenAuthenticationFilterTest {
         when(tppErrorMessageBuilder.buildTppErrorMessage(MessageCategory.ERROR, MessageErrorCode.TOKEN_INVALID))
                 .thenReturn(tppErrorMessage);
 
-        String errorMessage = new ObjectMapper().writeValueAsString(tppErrorMessage);
-
         // When
         tokenAuthenticationFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
 
         // Then
         verify(httpServletResponse).setStatus(HttpServletResponse.SC_FORBIDDEN);
         verify(httpServletResponse).setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        verify(mockWriter).print(errorMessage);
+        verify(mockWriter).print(tppErrorMessage.toString());
         verify(filterChain, never()).doFilter(any(), any());
     }
 
     @Test
     public void doFilterInternalTest_withBlankTokenInHeader_shouldReturnError() throws ServletException, IOException {
         // Given
-        when(httpServletRequest.getHeader(OAUTH_MODE_HEADER_NAME)).thenReturn("true");
+        when(httpServletRequest.getHeader(OAUTH_MODE_HEADER_NAME)).thenReturn(OAUTH_MODE_INTEGRATED);
         when(httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION))
                 .thenReturn("");
-        when(httpServletRequest.getServletPath()).thenReturn("/v1/accounts");
+        when(httpServletRequest.getServletPath()).thenReturn(SERVLET_PATH);
+
+        when(aspspProfileService.getScaApproaches()).thenReturn(Arrays.asList(ScaApproach.REDIRECT, ScaApproach.OAUTH));
+
+        Map<PaymentType, Set<String>> supportedPaymentTypeAndProductMatrix = new HashMap<>();
+        supportedPaymentTypeAndProductMatrix.put(PaymentType.SINGLE, Collections.singleton("sepa-credit-transfers"));
+        when(aspspProfileService.getAspspSettings()).thenReturn(new AspspSettings(null, new PisAspspProfileSetting(supportedPaymentTypeAndProductMatrix, 0, 0, false, null, null), null, null));
 
         PrintWriter mockWriter = Mockito.mock(PrintWriter.class);
         when(httpServletResponse.getWriter()).thenReturn(mockWriter);
@@ -141,25 +179,29 @@ public class TokenAuthenticationFilterTest {
         when(tppErrorMessageBuilder.buildTppErrorMessage(MessageCategory.ERROR, MessageErrorCode.TOKEN_INVALID))
                 .thenReturn(tppErrorMessage);
 
-        String errorMessage = new ObjectMapper().writeValueAsString(tppErrorMessage);
-
         // When
         tokenAuthenticationFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
 
         // Then
         verify(httpServletResponse).setStatus(HttpServletResponse.SC_FORBIDDEN);
         verify(httpServletResponse).setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        verify(mockWriter).print(errorMessage);
+        verify(mockWriter).print(tppErrorMessage.toString());
         verify(filterChain, never()).doFilter(any(), any());
     }
 
     @Test
     public void doFilterInternalTest_withNoBearerPrefixInHeader_shouldReturnError() throws ServletException, IOException {
         // Given
-        when(httpServletRequest.getHeader(OAUTH_MODE_HEADER_NAME)).thenReturn("true");
+        when(httpServletRequest.getHeader(OAUTH_MODE_HEADER_NAME)).thenReturn(OAUTH_MODE_INTEGRATED);
         when(httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION))
                 .thenReturn(BEARER_TOKEN_VALUE);
-        when(httpServletRequest.getServletPath()).thenReturn("/v1/accounts");
+        when(httpServletRequest.getServletPath()).thenReturn(SERVLET_PATH);
+
+        when(aspspProfileService.getScaApproaches()).thenReturn(Arrays.asList(ScaApproach.REDIRECT, ScaApproach.OAUTH));
+
+        Map<PaymentType, Set<String>> supportedPaymentTypeAndProductMatrix = new HashMap<>();
+        supportedPaymentTypeAndProductMatrix.put(PaymentType.SINGLE, Collections.singleton("sepa-credit-transfers"));
+        when(aspspProfileService.getAspspSettings()).thenReturn(new AspspSettings(null, new PisAspspProfileSetting(supportedPaymentTypeAndProductMatrix, 0, 0, false, null, null), null, null));
 
         PrintWriter mockWriter = Mockito.mock(PrintWriter.class);
         when(httpServletResponse.getWriter()).thenReturn(mockWriter);
@@ -168,15 +210,13 @@ public class TokenAuthenticationFilterTest {
         when(tppErrorMessageBuilder.buildTppErrorMessage(MessageCategory.ERROR, MessageErrorCode.TOKEN_INVALID))
                 .thenReturn(tppErrorMessage);
 
-        String errorMessage = new ObjectMapper().writeValueAsString(tppErrorMessage);
-
         // When
         tokenAuthenticationFilter.doFilter(httpServletRequest, httpServletResponse, filterChain);
 
         // Then
         verify(httpServletResponse).setStatus(HttpServletResponse.SC_FORBIDDEN);
         verify(httpServletResponse).setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        verify(mockWriter).print(errorMessage);
+        verify(mockWriter).print(tppErrorMessage.toString());
         verify(filterChain, never()).doFilter(any(), any());
     }
 }
