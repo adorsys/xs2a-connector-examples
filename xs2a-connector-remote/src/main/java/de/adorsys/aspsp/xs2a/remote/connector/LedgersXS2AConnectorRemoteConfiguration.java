829809cdd14463e4752089613e06eac72817971f
Serhii Shymkov <ssh@adorsys.com.ua>
2019-11-14 14:35:53 +0000

    Create starter with embedded CMS and ASPSP profile

--- xs2a-connector-remote/src/main/java/de/adorsys/aspsp/xs2a/remote/connector/LedgersXS2AConnectorRemoteConfiguration.java
+++ xs2a-connector-remote/src/main/java/de/adorsys/aspsp/xs2a/remote/connector/LedgersXS2AConnectorRemoteConfiguration.java
@@ -1,15 +1,11 @@
 package de.adorsys.aspsp.xs2a.remote.connector;
 
+import de.adorsys.aspsp.xs2a.connector.EnableLedgersXS2AConnector;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-import de.adorsys.aspsp.xs2a.connector.EnableLedgersXS2AConnector;
-import de.adorsys.aspsp.xs2a.remote.connector.spi.web.Xs2aInterfaceConfig;
 
 @Configuration
-@ComponentScan(basePackageClasses= {LedgersXS2AConnectorRemoteBasePackage.class})
+@ComponentScan(basePackageClasses = {LedgersXS2AConnectorRemoteBasePackage.class})
 @EnableLedgersXS2AConnector
-@Import(Xs2aInterfaceConfig.class)
 public class LedgersXS2AConnectorRemoteConfiguration {
 }
---
+++ xs2a-connector/src/test/java/de/adorsys/aspsp/xs2a/connector/spi/impl/CmsPaymentStatusUpdateServiceTest.java
@@ -0,0 +1,112 @@
+package de.adorsys.aspsp.xs2a.connector.spi.impl;
+
+import de.adorsys.aspsp.xs2a.connector.cms.CmsPsuPisClient;
+import de.adorsys.ledgers.middleware.api.domain.sca.SCALoginResponseTO;
+import de.adorsys.ledgers.middleware.api.domain.sca.ScaStatusTO;
+import de.adorsys.ledgers.middleware.api.service.TokenStorageService;
+import de.adorsys.psd2.xs2a.core.pis.TransactionStatus;
+import de.adorsys.psd2.xs2a.spi.domain.SpiAspspConsentDataProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CmsPaymentStatusUpdateServiceTest {
+    private static final String PAYMENT_ID = "some payment id";
+    private static final String INSTANCE_ID = "UNDEFINED";
+    private static final byte[] ASPSP_CONSENT_DATA = "some ASPSP consent Data".getBytes();
+
+    @Mock
+    private SpiAspspConsentDataProvider spiAspspConsentDataProvider;
+    @Mock
+    private TokenStorageService tokenStorageService;
+    @Mock
+    private CmsPsuPisClient cmsPsuPisClient;
+
+    @InjectMocks
+    private CmsPaymentStatusUpdateService cmsPaymentStatusUpdateService;
+
+    @Test
+    public void updatePaymentStatus_withIdentifiedAuthorisation_shouldUpdateToAccp() throws IOException {
+        // Given
+        when(spiAspspConsentDataProvider.loadAspspConsentData())
+                .thenReturn(ASPSP_CONSENT_DATA);
+        SCALoginResponseTO scaLoginResponse = new SCALoginResponseTO();
+        scaLoginResponse.setScaStatus(ScaStatusTO.PSUIDENTIFIED);
+        when(tokenStorageService.fromBytes(ASPSP_CONSENT_DATA)).thenReturn(scaLoginResponse);
+
+        // When
+        cmsPaymentStatusUpdateService.updatePaymentStatus(PAYMENT_ID, spiAspspConsentDataProvider);
+
+        // Then
+        verify(cmsPsuPisClient).updatePaymentStatus(PAYMENT_ID, TransactionStatus.ACCP, INSTANCE_ID);
+    }
+
+    @Test
+    public void updatePaymentStatus_withExemptedAuthorisation_shouldUpdateToAccp() throws IOException {
+        // Given
+        when(spiAspspConsentDataProvider.loadAspspConsentData())
+                .thenReturn(ASPSP_CONSENT_DATA);
+        SCALoginResponseTO scaLoginResponse = new SCALoginResponseTO();
+        scaLoginResponse.setScaStatus(ScaStatusTO.EXEMPTED);
+        when(tokenStorageService.fromBytes(ASPSP_CONSENT_DATA)).thenReturn(scaLoginResponse);
+
+        // When
+        cmsPaymentStatusUpdateService.updatePaymentStatus(PAYMENT_ID, spiAspspConsentDataProvider);
+
+        // Then
+        verify(cmsPsuPisClient).updatePaymentStatus(PAYMENT_ID, TransactionStatus.ACCP, INSTANCE_ID);
+    }
+
+    @Test
+    public void updatePaymentStatus_withAuthenticatedAuthorisation_shouldUpdateToActc() throws IOException {
+        // Given
+        when(spiAspspConsentDataProvider.loadAspspConsentData())
+                .thenReturn(ASPSP_CONSENT_DATA);
+        SCALoginResponseTO scaLoginResponse = new SCALoginResponseTO();
+        scaLoginResponse.setScaStatus(ScaStatusTO.PSUAUTHENTICATED);
+        when(tokenStorageService.fromBytes(ASPSP_CONSENT_DATA)).thenReturn(scaLoginResponse);
+
+        // When
+        cmsPaymentStatusUpdateService.updatePaymentStatus(PAYMENT_ID, spiAspspConsentDataProvider);
+
+        // Then
+        verify(cmsPsuPisClient).updatePaymentStatus(PAYMENT_ID, TransactionStatus.ACTC, INSTANCE_ID);
+    }
+
+    @Test
+    public void updatePaymentStatus_withOtherAuthorisationStatus_shouldUpdateToRcvd() throws IOException {
+        // Given
+        when(spiAspspConsentDataProvider.loadAspspConsentData())
+                .thenReturn(ASPSP_CONSENT_DATA);
+        SCALoginResponseTO scaLoginResponse = new SCALoginResponseTO();
+        scaLoginResponse.setScaStatus(ScaStatusTO.STARTED);
+        when(tokenStorageService.fromBytes(ASPSP_CONSENT_DATA)).thenReturn(scaLoginResponse);
+
+        // When
+        cmsPaymentStatusUpdateService.updatePaymentStatus(PAYMENT_ID, spiAspspConsentDataProvider);
+
+        // Then
+        verify(cmsPsuPisClient).updatePaymentStatus(PAYMENT_ID, TransactionStatus.RCVD, INSTANCE_ID);
+    }
+
+    @Test
+    public void updatePaymentStatus_withExceptionOnReadingToken_shouldSkipUpdate() throws IOException {
+        // Given
+        when(spiAspspConsentDataProvider.loadAspspConsentData())
+                .thenReturn(ASPSP_CONSENT_DATA);
+        when(tokenStorageService.fromBytes(ASPSP_CONSENT_DATA)).thenThrow(IOException.class);
+
+        // When
+        cmsPaymentStatusUpdateService.updatePaymentStatus(PAYMENT_ID, spiAspspConsentDataProvider);
+
+        // Then
+        verify(cmsPsuPisClient, never()).updatePaymentStatus(any(), any(), any());
+    }
+}
