fbf8da35f08bfbb912d81a9e58bc151b654666bf
Andrii Murashkin <amu@adorsys.com.ua>
2020-03-27 15:37:03 +0200

    #1250 Fixed processing multiple access headers

--- xs2a-connector/src/main/java/de/adorsys/aspsp/xs2a/connector/spi/impl/CardAccountSpiImpl.java
+++ xs2a-connector/src/main/java/de/adorsys/aspsp/xs2a/connector/spi/impl/CardAccountSpiImpl.java
@@ -215,9 +215,10 @@ public class CardAccountSpiImpl implements CardAccountSpi {
         }
     }
 
-    private String processAcceptMediaType(String acceptMediaType) {
-        return StringUtils.isBlank(acceptMediaType) || WILDCARD_ACCEPT_HEADER.equals(acceptMediaType) ?
-                       DEFAULT_ACCEPT_MEDIA_TYPE : acceptMediaType;
+    String processAcceptMediaType(String acceptMediaType) {
+        return StringUtils.isBlank(acceptMediaType)
+                       || WILDCARD_ACCEPT_HEADER.equals(acceptMediaType)
+                       || acceptMediaType.contains(",") ? DEFAULT_ACCEPT_MEDIA_TYPE : acceptMediaType;
     }
 
     @Override
