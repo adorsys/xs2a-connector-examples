d1dacdf32fdc7f2330a54cff8000b8a17d8dc16d
kyaex <kya@adorsys.com.ua>
2020-10-01 18:01:09 +0300

    #1355 added listeners to embedded connector

---
+++ gateway-app-embedded/src/main/java/de/adorsys/ledgers/gateway/config/HibernateListenerConfig.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018-2020 adorsys GmbH & Co KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.adorsys.ledgers.gateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.event.service.spi.EventListenerRegistry;
+import org.hibernate.event.spi.EventType;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+@Configuration
+@RequiredArgsConstructor
+public class HibernateListenerConfig {
+    @PersistenceUnit
+    private EntityManagerFactory entityManagerFactory;
+
+    private final ServiceInstanceIdEventListener serviceInstanceIdEventListener;
+
+    @PostConstruct
+    public void registerListeners() {
+        final SessionFactoryImplementor sessionFactory = entityManagerFactory.unwrap(SessionFactoryImplementor.class);
+
+        final EventListenerRegistry registry = sessionFactory
+                                                   .getServiceRegistry()
+                                                   .getService(EventListenerRegistry.class);
+
+        registry.getEventListenerGroup(EventType.PRE_INSERT)
+            .appendListener(serviceInstanceIdEventListener);
+    }
+}
